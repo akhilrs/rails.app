a9ff49bb34c00ced7e12af1f9c133446b68e7062
Akhil R S <akhilrs@live.com>
2013-12-04 22:51:17 +0530

    Added a Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsApp::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
