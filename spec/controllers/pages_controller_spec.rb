41e50732e2efc4b7dc724a48f7c75e28bfeffc4c
Akhil R S <akhilrs@live.com>
2013-12-05 00:14:39 +0530

    Added a Site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       expect(response).to be_success
+    end
+    it "should have a right title" do
+      get "home"
+      expect(response).should have_selector("title", :content => "Rails.App | Home")
+    end
+    it "should have a non-blank body" do
+      get "home"
+      expect(response).body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -13,6 +22,14 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       expect(response).to be_success
+    end
+    it "should have a right title" do
+      get "contact"
+      expect(response).should have_selector("title", :content => "Rails.App | Contact")
+    end
+     it "should have a non-blank body" do
+      get "contact"
+      expect(response).body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
